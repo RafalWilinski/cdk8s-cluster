c983036d71fb7889a22b2bc8c54a6b0b5dbabcc9
Rafal Wilinski <raf.wilinski@gmail.com>
2020-03-21 08:19:35 +0100

    Initial commit

--- bin/aws-cdk-k8s-fargate.ts
+++ bin/aws-cdk-k8s-fargate.ts
@@ -1,7 +1,10 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from '@aws-cdk/core';
-import { AwsCdkK8SFargateStack } from '../lib/aws-cdk-k8s-fargate-stack';
+import "source-map-support/register";
+import * as cdk from "@aws-cdk/core";
+import { AwsCdkK8SFargateStack } from "../lib/aws-cdk-k8s-fargate-stack";
 
+const env = {
+  region: "us-east-1"
+};
 const app = new cdk.App();
-new AwsCdkK8SFargateStack(app, 'AwsCdkK8SFargateStack');
+new AwsCdkK8SFargateStack(app, "AwsCdkK8SFargateStack", { env });
--- lib/aws-cdk-k8s-fargate-stack.ts
+++ lib/aws-cdk-k8s-fargate-stack.ts
@@ -1,9 +1,46 @@
-import * as cdk from '@aws-cdk/core';
+import * as cdk from "@aws-cdk/core";
+import * as eks from "@aws-cdk/aws-eks";
+import * as iam from "@aws-cdk/aws-iam";
+
+import HelloKubernetesChart from "../main";
 
 export class AwsCdkK8SFargateStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const namespaceSelector = { namespace: "fargate" };
+
+    const clusterAdmin = new iam.Role(this, "AdminRole", {
+      assumedBy: new iam.AccountRootPrincipal(),
+      managedPolicies: [
+        iam.ManagedPolicy.fromAwsManagedPolicyName("AmazonEKSServicePolicy"),
+        iam.ManagedPolicy.fromAwsManagedPolicyName("AmazonEKSClusterPolicy")
+      ]
+    });
+
+    const policyStatement = new iam.PolicyStatement();
+    policyStatement.addAllResources();
+    policyStatement.addActions(
+      "elasticloadbalancing:*",
+      "ec2:CreateSecurityGroup",
+      "ec2:Describe*"
+    );
+
+    clusterAdmin.addToPolicy(policyStatement);
+
+    const cluster = new eks.Cluster(this, "k8s-eks-fargate-cluster", {
+      clusterName: "k8s-eks-cluster",
+      outputConfigCommand: true,
+      mastersRole: clusterAdmin,
+      version: "1.15",
+      defaultCapacity: 0
+    });
+
+    cluster.addFargateProfile("k8s-eks-cluster-fargate-profile", {
+      selectors: [namespaceSelector]
+    });
+
+    const chart = new HelloKubernetesChart(this, "HelloChart");
+    cluster.addResource("hello-chart", ...chart.toJson());
   }
 }
